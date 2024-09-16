7acd9d2afbb950d11445af0175cc7b7dea85646a
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 17:13:22 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -3,14 +3,12 @@ setInterval(function() {
    $("#ff_UserYesNo_02").addClass('checkbox');
      setTimeout(function() {
         $("#ff_UserYesNo_02").removeClass('checkbox');
-        document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');"
      }, 500);
  }, 10);
 document.getElementsByClassName('slds-textarea')[9].style.height = '350';
-function myFunction() {
 if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
     {
       document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');")
       }
 
-}
+
