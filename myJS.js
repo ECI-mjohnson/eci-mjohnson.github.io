9f21e72812a059086fa6ef6d7f107c3a72be1da9
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 17:08:16 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -3,7 +3,7 @@ setInterval(function() {
    $("#ff_UserYesNo_02").addClass('checkbox');
      setTimeout(function() {
         $("#ff_UserYesNo_02").removeClass('checkbox');
-        myFunction();
+        document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');"
      }, 500);
  }, 1000);
 document.getElementsByClassName('slds-textarea')[9].style.height = '350';
