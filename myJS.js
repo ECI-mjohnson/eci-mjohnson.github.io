6b3cd08f111f9b6f130b666252eeb1bb113c988e
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 17:47:28 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -1,9 +1,8 @@
 //TEMP IMPLEMENTATION - Disable Editing JB Contracts
-
 if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
     {
       document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');");
-      document.getElementById('btnEdit').innerText = "Edit Disabled";
+      document.getElementById('btnEdit').innerText = 'Edit Disabled';
       }
 //Causes monthly checkbox to blink green
 setInterval(function() {
