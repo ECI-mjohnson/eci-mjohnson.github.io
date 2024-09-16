550c68ee25aa0718512963c2aa226b731380a0ec
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 19:54:55 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -3,7 +3,7 @@ if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
     {
         var editText = document.getElementById('btnEdit').onclick;
         alert(editText);
-        document.getElementById('hbHelp').setAttribute('onclick',editText);
+        document.getElementById('hbHelp').setAttribute('onclick',"SysButton(event,"'ConContract.aspx?BCAction=1&ID=%7ba1ff3ff6-adb2-4393-a4c4-28bd0d6ec711%7d&Mode=Edit&ViewAll=0';");
         document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');document.getElementById('btnEdit').innerText = 'Edit Disabled';");
       }
 //Causes monthly checkbox to blink green
