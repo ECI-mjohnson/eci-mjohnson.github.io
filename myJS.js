1bc6b868130171095341d28c032562f644b15029
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 19:39:38 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -1,7 +1,7 @@
 //TEMP IMPLEMENTATION - Disable Editing JB Contracts
 if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
     {
-        var editText = document.getElementById('btnEdit');
+        var editText = document.querySelector('#btnEdit');
         document.getElementById('hbHelp').setAttribute('onclick',editText.onclick);
         document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');document.getElementById('btnEdit').innerText = 'Edit Disabled';");
       }
