f87265e7334373c004f5a04f20e1b04edf2f6244
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 17:03:44 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -6,3 +6,10 @@ setInterval(function() {
      }, 500);
  }, 1000);
 document.getElementsByClassName('slds-textarea')[9].style.height = '350';
+function myFunction() {
+if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
+    {
+      document.getElementById('bob').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');")
+      }
+
+}
