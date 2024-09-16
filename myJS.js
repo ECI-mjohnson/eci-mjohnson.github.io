8db3c83b38a9f2363473a9cde463ba65ac936a69
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-09-16 17:16:03 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -1,14 +1,13 @@
+//TEMP IMPLEMENTATION - Disable Editing JB Contracts
+if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
+    {
+      document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');")
+      }
 //Causes monthly checkbox to blink green
 setInterval(function() {
    $("#ff_UserYesNo_02").addClass('checkbox');
      setTimeout(function() {
         $("#ff_UserYesNo_02").removeClass('checkbox');
      }, 500);
- }, 10);
+ }, 1000);
 document.getElementsByClassName('slds-textarea')[9].style.height = '350';
-if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
-    {
-      document.getElementById('btnEdit').setAttribute('onclick',"alert('I hearby declare that the editing of this contract is not allowed');")
-      }
-
-
