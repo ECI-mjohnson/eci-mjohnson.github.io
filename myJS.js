5c82bdba6b914dc98f1f3b886d5521898dcf43a0
ECI-mjohnson <146053160+ECI-mjohnson@users.noreply.github.com>
2024-11-11 11:03:58 -0500

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -4,7 +4,7 @@ if (document.getElementById('Assortment_ref').innerText == 'JobBOSS')
         // var editText = document.getElementById('btnEdit').getAttribute('onclick');
         // document.getElementById('hbHelp').setAttribute('onclick',editText);
         // document.getElementById('btnEdit').setAttribute('onclick',"alert('I hereby declare that the editing of this contract is not allowed');document.getElementById('btnEdit').innerText = 'Edit Disabled';");
-        document.getElementById('btnEdit').innerText = '-Edit-';
+        document.getElementById('btnEdit').innerText = 'Edit';
       }
 //Causes monthly checkbox to blink green
 setInterval(function() {
